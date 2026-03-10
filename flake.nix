2cb1ab1774c37f3a6a0fe79cd1fde351a1a3d413
mike <hey@mike.ee>
2026-03-10 21:55:18 +0000

    chore: update target to unstable channel
    Signed-off-by: mike <hey@mike.ee>

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "Multi-machine NixOS flake with Home Manager, roles, overlays, and CI";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-    home-manager.url = "github:nix-community/home-manager/release-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
