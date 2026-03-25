704bdc80ce2002c2f7999748abd3c045fd5539d6
mike <nix@mike.ee>
2026-03-25 16:36:24 +0000

    chore: update home-manager configuration for user 'mike' to include monitors.xml with force option

--- hosts/vm-aarch64/default.nix
+++ hosts/vm-aarch64/default.nix
@@ -21,6 +21,35 @@
   boot.loader.systemd-boot = {
     enable = true;
   };
+
+  home-manager.users.mike = {
+    xdg.configFile."monitors.xml".text = ''
+      <monitors version="2">
+        <configuration>
+          <layoutmode>physical</layoutmode>
+          <logicalmonitor>
+            <x>0</x>
+            <y>0</y>
+            <scale>2</scale>
+            <primary>yes</primary>
+            <monitor>
+              <monitorspec>
+                <connector>Virtual-1</connector>
+                <vendor>unknown</vendor>
+                <product>unknown</product>
+                <serial>unknown</serial>
+              </monitorspec>
+              <mode>
+                <width>4112</width>
+                <height>2580</height>
+                <rate>60.000</rate>
+              </mode>
+            </monitor>
+          </logicalmonitor>
+        </configuration>
+      </monitors>
+    '';
+  };
   
   boot.loader.efi.canTouchEfiVariables = false;
 }
