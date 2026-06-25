251c13f3683d959ba6bfaf32ae7701ff13be4edb
Mike Nguyen <hey@mike.ee>
2026-06-25 15:08:18 +0100

    add arm64 to copilot overlay
    Signed-off-by: Mike Nguyen <hey@mike.ee>

--- overlays/github-copilot-cli.nix
+++ overlays/github-copilot-cli.nix
@@ -10,6 +10,10 @@ let
       name = "linux-x64";
       hash = "sha256-XZAUZRrVHbbz/r5YSVSgoZWz2ZfQ7Y9ZrmdJjWMKdd8=";
     };
+    aarch64-linux = {
+      name = "linux-arm64";
+      hash = "sha512-dOwdy/YbTXQN/+x2v4ZgiDycdRtWElyHxPuA6ail3yJDt0nagwn8OYAA/diBLPMAJuuBXiOZGvvb9fGRuh7Xgg==";
+    };
   }.${stdenv.hostPlatform.system}
     or (throw "github-copilot-cli overlay: unsupported system ${stdenv.hostPlatform.system}");
 in
