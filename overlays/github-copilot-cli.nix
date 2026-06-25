90e3aadda6bf96d02c88b8faee91e037c9649f3a
Mike Nguyen <hey@mike.ee>
2026-06-25 12:45:47 +0100

    chore: bump deps (#6)
    * chore: bump deps
    
    Signed-off-by: Mike Nguyen <hey@mike.ee>
    
    * fix: resolve claude mismatch
    
    Signed-off-by: Mike Nguyen <hey@mike.ee>
    
    ---------
    
    Signed-off-by: Mike Nguyen <hey@mike.ee>

--- overlays/github-copilot-cli.nix
+++ overlays/github-copilot-cli.nix
@@ -1,9 +1,9 @@
 final: prev: {
   github-copilot-cli = prev.github-copilot-cli.overrideAttrs (finalAttrs: previousAttrs: {
-    version = "1.0.62";
+    version = "1.0.65";
     src = final.fetchurl {
       url = "https://github.com/github/copilot-cli/releases/download/v${finalAttrs.version}/github-copilot-${finalAttrs.version}.tgz";
-      hash = "sha256-YADhvEnBR89CnyMK1b/Q39mAn03iuGbG70C+umfd+fI=";
+      hash = "sha256-jn4lN6LZsqjSUdzvmqREnOYJJQFcRc2cpXaKEw5lytU=";
     };
     autoPatchelfIgnoreMissingDeps = previousAttrs.autoPatchelfIgnoreMissingDeps ++ [
       "libc.musl-x86_64.so.1"
